ee1339965b2c90a876835ae0d8a92e6c13829577
Sandeep Thukral <sandeep.thukral@insided.com>
2020-03-02 17:45:00 +0100

    adding config to base class

--- src/test/java/sandeep/qa/base/TestBase.java
+++ src/test/java/sandeep/qa/base/TestBase.java
@@ -4,28 +4,35 @@ import com.codeborne.selenide.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class TestBase {
 
     public static Properties prop;
-
-    //TODO instantiate an object that reads data from the props file. Not using props.getPRoperty() everywhere
+    public static Map<String, String> config;
 
     public TestBase(){
-        try{
+        try {
             if (null == prop) {
+                // Read the properties file
                 prop = new Properties();
                 FileInputStream fis = new FileInputStream("src/test/java/sandeep/qa/config.properties");
                 prop.load(fis);
+
+                // feed this to a hashmap
+                config = new HashMap<String, String>();
+                for (final String name : prop.stringPropertyNames())
+                    config.put(name, prop.getProperty(name));
             }
-        } catch (IOException e) {
+        }catch (IOException e) {
             e.printStackTrace();
         }
 
         Configuration.startMaximized = false;
-        Configuration.browser = prop.getProperty("browser", "chrome");
-        Configuration.browserSize = prop.getProperty("browser.size", "1280x1024");
-        Configuration.baseUrl = prop.getProperty("base_url");
+        Configuration.browser = config.get("browser");
+        Configuration.browserSize = config.get("browser.size");
+        Configuration.baseUrl = config.get("base_url");
     }
 }
